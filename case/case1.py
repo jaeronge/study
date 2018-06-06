ac2890fda61cfa7e9acb8b1e0fac7f8d0675c0bf
jaeronge <jaeronge@gmail.com>
2018-06-06 21:43:00 +0900

    add cases

--- case/case1.py
+++ case/case1.py
@@ -1,5 +1,5 @@
 
-def kaka_test1():
+def kaka_map():
     n = 5
     al = [9, 20, 28, 18, 11]
     bl = [30, 1, 21, 17, 28]
@@ -14,7 +14,7 @@ def kaka_test1():
     print(rl)
 
 
-def kaka_test2(in_str):
+def kaka_dart(in_str):
     result = []
     point = None
     point_list = ["0","1","2","3","4","5","6","7","8","9"]
@@ -47,10 +47,11 @@ def kaka_test2(in_str):
     print(sum(result))
 
 
-def kaka_test3(c_size, citys):
+def kaka_cache(c_size, citys):
     cache = []
     cal_time = 0
     for c in citys:
+        c = c.lower()
         if c in cache:
             cal_time += 1
         else:
@@ -62,22 +63,64 @@ def kaka_test3(c_size, citys):
     print(cal_time)
 
 
-def kaka_test4(n, t, m, timetable):
+def kaka_bus(n, t, m, timetable):
     print("start----")
     rl = list(int(time.split(":")[0])*60 + int(time.split(":")[1]) for time in timetable)
     rl.sort()
     print(rl)
+    last_bus_c = 0
+    last_ride_time = 0
     for num in range(0, n):
         now = 540 + num * t
         pop_count = 0
         tmp_rl = rl.copy()
+        last_bus_c = 0
         for r in rl:
             if r <= now and pop_count < m:
                 tmp_rl.pop(tmp_rl.index(r))
                 pop_count += 1
-                print("pop:", r)
-                print("tmp_rl:", tmp_rl)
-            else:
-                print("non-pop:", r)
+                # print("pop:", r)
+                last_bus_c += 1
+                last_ride_time = r
         rl = tmp_rl
-        print("remain:", rl)
+        # print("remain:", rl)
+
+    if last_bus_c < m:  # can ride.
+        # print("Remain!!", m - last_bus_c)
+        last_min = 540 + (n-1) * t
+    else:
+        last_min = last_ride_time - 1
+        # print("Full!!")
+
+    print("--Result: {0:02n}:{1:02n}".format(int(last_min / 60), last_min % 60))
+
+
+def jaccard(left, right):
+    left = left.lower()
+    right = right.lower()
+
+    left_pairs = _pair_list(left)
+    right_pairs = _pair_list(right)
+    print("{},{}".format(left_pairs, right_pairs))
+    n_count = 0
+    u_count = 0
+    for l in left_pairs:
+        if l in right_pairs:
+            n_count += 1
+    u_count = len(left_pairs + right_pairs) - n_count
+    print("{},{}".format(n_count, u_count))
+    if u_count == 0:
+        jn = 1
+    else:
+        jn = n_count / u_count
+    print("Result:", int(jn * 65536))
+
+
+def _pair_list(s):
+    pairs = []
+    for l in range(0, len(s) - 1):
+        pair = s[l] + s[l + 1]
+        if pair.isalpha() is False:
+            continue
+        pairs.append(pair)
+    return pairs
--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 # -*- coding: utf-8 -*-
 
-from test_case import test_case_1
+from case import case1
 
 
 if __name__ == "__main__":
-    test_case_1.kaka_test4(1,1,5,["08:00", "08:01", "08:02", "08:03"])  # 	"09:00"
-    test_case_1.kaka_test4(2,10,2,["09:10", "09:09", "08:00"])  # 	"09:09"
-    test_case_1.kaka_test4(2,1,2,["09:00", "09:00", "09:00", "09:00"])  # 	"08:59"
-    test_case_1.kaka_test4(1,1,5,["00:01", "00:01", "00:01", "00:01", "00:01"])  # 	"00:00"
-    test_case_1.kaka_test4(1,1,1,["23:59"])  # 	"09:00"
-    test_case_1.kaka_test4(10,60,45,["23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"]) # "18:00"
+    case1.jaccard("FRANCE", "french")    # 16384
+    case1.jaccard("handshake", "shake hands")    # 65536
+    case1.jaccard("aa1 + aa2", "AAAA12")    # 43690
+    case1.jaccard("E = M * C ^ 2", "e = m * c ^ 2")    #65536
 
-    test_case_1.kaka_test3(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]) # 50
-    test_case_1.kaka_test3(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]) #	21
-    test_case_1.kaka_test3(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]) #	60
-    test_case_1.kaka_test3(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]) #	52
-    test_case_1.kaka_test3(2, ["Jeju", "Pangyo", "NewYork", "newyork"]) #	16
-    test_case_1.kaka_test3(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"])    #25
+    # case1.kaka_bus(1,1,5,["08:00", "08:01", "08:02", "08:03"])  # 	"09:00"
+    # case1.kaka_bus(2,10,2,["09:10", "09:09", "08:00"])  # 	"09:09"
+    # case1.kaka_bus(2,1,2,["09:00", "09:00", "09:00", "09:00"])  # 	"08:59"
+    # case1.kaka_bus(1,1,5,["00:01", "00:01", "00:01", "00:01", "00:01"])  # 	"00:00"
+    # case1.kaka_bus(1,1,1,["23:59"])  # 	"09:00"
+    # case1.kaka_bus(10,60,45,["23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"]) # "18:00"
 
-    test_case_1.kaka_test2("1S2D*3T")   # 37
-    test_case_1.kaka_test2("1D2S#10S")  # 9
-    test_case_1.kaka_test2("1D2S0T")    # 3
-    test_case_1.kaka_test2("1S*2T*3S")  # 23
-    test_case_1.kaka_test2("1D#2S*3S")  # 5
-    test_case_1.kaka_test2("1T2D3D#")   # -4
-    test_case_1.kaka_test2("1D2S3T*")   # 59
+    # case1.kaka_cache(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]) # 50
+    # case1.kaka_cache(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]) #	21
+    # case1.kaka_cache(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]) #	60
+    # case1.kaka_cache(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]) #	52
+    # case1.kaka_cache(2, ["Jeju", "Pangyo", "NewYork", "newyork"]) #	16
+    # case1.kaka_cache(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"])    #25
+    #
+    # case1.kaka_dart("1S2D*3T")   # 37
+    # case1.kaka_dart("1D2S#10S")  # 9
+    # case1.kaka_dart("1D2S0T")    # 3
+    # case1.kaka_dart("1S*2T*3S")  # 23
+    # case1.kaka_dart("1D#2S*3S")  # 5
+    # case1.kaka_dart("1T2D3D#")   # -4
+    # case1.kaka_dart("1D2S3T*")   # 5
+    #
+    # case1.kaka_map()
